333c7ba7e87bfadc3693d6bcab64258a5e63d482
Dlndavis020 <dlndavis020@gmail.com>
2025-03-17 16:26:05 +0000

    Created All Classes Files and Created GUI TODO// Add code to all classes

---
+++ src/main/java/dylan/projects/bloodtestscheduler/BloodTestScheduler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dylan.projects.bloodtestscheduler;
+
+/**
+ *
+ * @author dlnda
+ */
+public class BloodTestScheduler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
